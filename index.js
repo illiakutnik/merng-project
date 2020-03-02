10fa5d4c75ad17e073c795cf505d7a7281c4225c
Illia KUtnik <illiakutnik@gmail.com>
2020-03-02 19:28:44 +0200

    server.part1

---
+++ index.js
@@ -0,0 +1,33 @@
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
+
+const { db } = require('./config')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+const Post = require('./models/Post')
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+})
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+			// useCreateIndex: true,
+			// useFindAndModify: false
+		})
+		console.log('mongo connected')
+	} catch (err) {
+		console.error(err.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
+
+server.listen({ port: 4000 }).then(res => {
+	console.log(`server running at ${res.url}`)
+})
